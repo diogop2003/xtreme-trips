45b34a666437e6ef7191b318778040b33e1d5b42
Heitor Bonanno <heitor.cb10@gmail.com>
2022-06-07 17:03:15 -0300

    aaa

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
   def index 
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:trail_id])
+  end
+
+  def edit
+    @event = Event.find(params[:trail_id])
+  end
+
+  def update
+    @event = Event.find(params[:trail_id])
+
+    if @event.update(event_params)
+      redirect_to events_path, notice: "Your events was updated!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :name)
+  end
 end
