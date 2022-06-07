2d04f0172c46259684d5843bbd0dbbee7864d725
Diogo Prado <diogoprado1511@gmail.com>
2022-06-07 16:26:00 -0300

    a

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
-  def index 
+  def index
     @events = Event.all
+  end
+
+  def new
+    @trail = Trail.find(params[:trail_id])
+    @event = Event.new
+  end
+
+  def create
+    @trail = Trail.find(params[:trail_id])
+    @event = Event.new(event_params)
+    @event.trail = @trail
+    if @event.save
+      redirect_to root_path, notice: "Your event was created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date)
   end
 end
