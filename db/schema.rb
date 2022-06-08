a4d033cd023f31890d1966e9aaf5f47044dc617a
raphael laranjeira <raphagobatto@hotmail.com>
2022-06-08 17:49:06 -0300

    edit btns and footer

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_194735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "checkpoints", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.boolean "start_point"
-    t.boolean "end_point"
-    t.bigint "trail_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["trail_id"], name: "index_checkpoints_on_trail_id"
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -98,7 +87,8 @@ ActiveRecord::Schema.define(version: 2022_06_08_194735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "checkpoints", "trails"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "event_users", "events"
   add_foreign_key "event_users", "users"
   add_foreign_key "events", "trails"
