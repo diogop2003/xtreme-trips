a9467898a0fc1055a1376104b050f98baac73f62
Daniel Carvalho <daluvijama@gmail.com>
2022-06-08 18:15:29 -0300

    edit sing-up

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_194735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "checkpoints", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.boolean "start_point"
-    t.boolean "end_point"
-    t.bigint "trail_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["trail_id"], name: "index_checkpoints_on_trail_id"
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -70,32 +61,14 @@ ActiveRecord::Schema.define(version: 2022_06_08_194735) do
     t.index ["trail_id"], name: "index_events_on_trail_id"
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "trails", force: :cascade do |t|
     t.string "mode"
     t.float "distance"
     t.string "name"
-    t.bigint "group_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.index ["group_id"], name: "index_trails_on_group_id"
     t.index ["user_id"], name: "index_trails_on_user_id"
-  end
-
-  create_table "user_groups", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "user_id", null: false
-    t.boolean "aproved"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_user_groups_on_group_id"
-    t.index ["user_id"], name: "index_user_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -114,12 +87,10 @@ ActiveRecord::Schema.define(version: 2022_06_08_194735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "checkpoints", "trails"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "event_users", "events"
   add_foreign_key "event_users", "users"
   add_foreign_key "events", "trails"
-  add_foreign_key "trails", "groups"
   add_foreign_key "trails", "users"
-  add_foreign_key "user_groups", "groups"
-  add_foreign_key "user_groups", "users"
 end
