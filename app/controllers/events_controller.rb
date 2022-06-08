2dbec39dd27466c9b4f073bb657d60f47f4f96c1
Heitor Bonanno <heitor.cb10@gmail.com>
2022-06-08 16:39:49 -0300

    event policy pundit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,41 +1,27 @@
 class EventsController < ApplicationController
   def index
     @trail = Trail.find(params[:trail_id]) 
-    @events = Event.where(trail_id: @trail)
+    @events = policy_scope(Event).where(trail: @trail)
+
   end
   
   def show
     @trail = Trail.find(params[:trail_id])
     @event = Event.find(params[:id])
-  end
-
-  def show
-    @event = Event.find(params[:trail_id])
-  end
-
-  def edit
-    @event = Event.find(params[:trail_id])
-  end
-
-  def update
-    @event = Event.find(params[:trail_id])
-
-    if @event.update(event_params)
-      redirect_to events_path, notice: "Your events was updated!"
-    else
-      render :edit
-    end
+    authorize @event
   end
   
   def new
     @trail = Trail.find(params[:trail_id])
     @event = Event.new
+    authorize @event
   end
 
   def create
     @trail = Trail.find(params[:trail_id])
     @event = Event.new(event_params)
     @event.trail = @trail
+    authorize @event
     if @event.save
       redirect_to root_path, notice: "Your event was created!"
     else
@@ -44,13 +30,13 @@ class EventsController < ApplicationController
   end
 
   def edit
-    @trail = Trail.find(params[:trail_id])
     @event = Event.find(params[:id])
+    authorize @event
   end
 
   def update
     @event = Event.find(params[:id])
-
+    authorize @event
     if @event.update(event_params)
       redirect_to root_path, notice: "Your events was updated!"
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trails, except: :destroy do
-    resources :events
+    resources :events, only: %i[index new create]
   end
+  resources :events, only: %i[show edit update destroy]
 end
