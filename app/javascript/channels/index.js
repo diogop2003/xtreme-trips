aeff5feee51416909a54451dd42ed7f844797ae5
Heitor Bonanno <heitor.cb10@gmail.com>
2022-06-10 11:02:35 -0300

    add mapbox

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,73 +4,61 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGVpdG9yLWNvY2EiLCJhIjoiY2w0N2Q1djh2MGI4czNkcnljcWEybGJpeiJ9.zMcxCyfzHYKdplZPO7OGfQ';
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
+center: [-122.486052, 37.830348], // starting position [lng, lat]
+zoom: 14 // starting zoom
 });
 
-map.on('load', async () => {
-// Get the initial location of the International Space Station (ISS).
-const geojson = await getLocation();
-// Add the ISS location as a source.
-map.addSource('iss', {
-type: 'geojson',
-data: geojson
-});
-// Add the rocket symbol layer to the map.
-map.addLayer({
-'id': 'iss',
-'type': 'symbol',
-'source': 'iss',
-'layout': {
-// This icon is a part of the Mapbox Streets style.
-// To view all images available in a Mapbox style, open
-// the style in Mapbox Studio and click the "Images" tab.
-// To add a new image to the style at runtime see
-// https://docs.mapbox.com/mapbox-gl-js/example/add-image/
-'icon-image': 'rocket-15'
-}
-});
-
-// Update the source from the API every 2 seconds.
-const updateSource = setInterval(async () => {
-const geojson = await getLocation(updateSource);
-map.getSource('iss').setData(geojson);
-}, 2000);
-
-async function getLocation(updateSource) {
-// Make a GET request to the API and return the location of the ISS.
-try {
-const response = await fetch(
-'https://api.wheretheiss.at/v1/satellites/25544',
-{ method: 'GET' }
-);
-const { latitude, longitude } = await response.json();
-// Fly the map to the location.
-map.flyTo({
-center: [longitude, latitude],
-speed: 0.5
-});
-// Return the location of the ISS as GeoJSON.
-return {
-'type': 'FeatureCollection',
-'features': [
-{
-'type': 'Feature',
-'geometry': {
-'type': 'Point',
-'coordinates': [longitude, latitude]
-}
-}
-]
-};
-} catch (err) {
-// If the updateSource interval is defined, clear the interval to stop updating the source.
-if (updateSource) clearInterval(updateSource);
-throw new Error(err);
-}
-}
-});
+map.on('load', () => {
+  map.addSource('route', {
+  'type': 'geojson',
+  'data': {
+  'type': 'Feature',
+  'properties': {},
+  'geometry': {
+  'type': 'LineString',
+  'coordinates': [
+  [-122.483696, 37.833818],
+  [-122.483482, 37.833174],
+  [-122.483396, 37.8327],
+  [-122.483568, 37.832056],
+  [-122.48404, 37.831141],
+  [-122.48404, 37.830497],
+  [-122.483482, 37.82992],
+  [-122.483568, 37.829548],
+  [-122.48507, 37.829446],
+  [-122.4861, 37.828802],
+  [-122.486958, 37.82931],
+  [-122.487001, 37.830802],
+  [-122.487516, 37.831683],
+  [-122.488031, 37.832158],
+  [-122.488889, 37.832971],
+  [-122.489876, 37.832632],
+  [-122.490434, 37.832937],
+  [-122.49125, 37.832429],
+  [-122.491636, 37.832564],
+  [-122.492237, 37.833378],
+  [-122.493782, 37.833683]
+  ]
+  }
+  }
+  });
+  map.addLayer({
+  'id': 'route',
+  'type': 'line',
+  'source': 'route',
+  'layout': {
+  'line-join': 'round',
+  'line-cap': 'round'
+  },
+  'paint': {
+  'line-color': '#46664D',
+  'line-width': 4
+  }
+  });
+  });
