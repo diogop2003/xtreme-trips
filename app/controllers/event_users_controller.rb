d849bee7ba4df8192f6eb0d9b5e41d77ab9af160
Diogo Prado <diogoprado1511@gmail.com>
2022-06-13 15:29:01 -0300

    Edit button and join event working

---
+++ app/controllers/event_users_controller.rb
@@ -0,0 +1,9 @@
+class EventUsersController < ApplicationController
+  def create
+    @event = Event.find(params["param"])
+    @event_user = EventUser.new(event: @event, user: current_user)
+    @event_user.save
+    redirect_to @event, notice: "You enter in the group!"
+    authorize @event_user
+  end
+end
