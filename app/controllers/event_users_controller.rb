e2d5ae42c0d39ddbaafb4c095f384d6c96f15739
Diogo Prado <diogoprado1511@gmail.com>
2022-06-13 16:57:11 -0300

    Join Finished

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -3,7 +3,7 @@ class EventUsersController < ApplicationController
     @event = Event.find(params["param"])
     @event_user = EventUser.new(event: @event, user: current_user)
     @event_user.save
-    redirect_to @event, notice: "You enter in the group!"
+    redirect_to @event, notice: "You enter in the event!"
     authorize @event_user
   end
 end
